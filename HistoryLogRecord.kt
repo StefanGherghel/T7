e9e833fb08b110d1a3a6715ac1965a375fcf4307
StefanGherghel <126242850+StefanGherghel@users.noreply.github.com>
2023-06-01 21:54:54 +0300

    Update HistoryLogRecord.kt

--- HistoryLogRecord.kt
+++ HistoryLogRecord.kt
@@ -13,7 +13,6 @@ class HistoryLogRecord(val time_stamp: Timestamp, val command: String): Comparab
 }
 
 
-//am creat o functie polimorfica care astepta ca parametrii doar clase care sunt subclase ale lui Comparable
 fun <T : Comparable<T>>maxim(first: T, second:T) : T{
     val k = first.compareTo(second)
     return if (k>0) first else second
@@ -27,7 +26,6 @@ class mapa<T>(val elements: MutableMap<Timestamp, T>)
 }
 
 
-//nu ar trebui contravarianta?????
 fun <T: HistoryLogRecord>change(old: T, new: T, my_map: MutableMap<Timestamp, in T>)
 {
     my_map.replace(old.time_stamp, old, new)
